fabdf049574a05ef8dd9984ca7ce2d067cc3d276
Polina <polmi713@gmail.com>
2021-08-24 01:25:41 +0300

    Fix hw 4.1 - remove recursive component nesting

--- 4/RainbowFrame/components/RainbowFrame.jsx
+++ 4/RainbowFrame/components/RainbowFrame.jsx
@@ -8,18 +8,35 @@ class RainbowFrame extends React.Component {
 		colors: PropTypes.arrayOf(PropTypes.string).isRequired,
 	}
 
+	// v1
+	// render() {
+	// 	const innerFrames = this.props.colors.slice(1);
+
+	// 	return(
+	// 		<div className='RainbowFrame' style={{borderColor: this.props.colors[0]}}>
+	// 			{
+	// 				innerFrames.length ?
+	// 				<RainbowFrame colors={innerFrames}>
+	// 					{this.props.children}
+	// 				</RainbowFrame> :
+	// 				this.props.children
+	// 			}
+	// 		</div>
+	// 	);
+	// }
+
+	// v2
 	render() {
 		const innerFrames = this.props.colors.slice(1);
+		let children = this.props.children;
 
-		return(
+		innerFrames.map(frameColor => {
+			children = <div className='RainbowFrame' style={{borderColor: frameColor}}>{children}</div>
+		});
+
+		return (
 			<div className='RainbowFrame' style={{borderColor: this.props.colors[0]}}>
-				{
-					innerFrames.length ?
-					<RainbowFrame colors={innerFrames}>
-						{this.props.children}
-					</RainbowFrame> :
-					this.props.children
-				}
+				{children}
 			</div>
 		);
 	}
